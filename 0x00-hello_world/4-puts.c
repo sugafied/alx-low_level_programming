e8af9b2dd4f86d53d889d9666ea64e65874bbc90
sugafied <nyanchuomosa@gmail.com>
2023-05-27 12:38:43 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char cisfun[] = "Programming is like building a multilingual puzzle";
+
+	puts(cisfun);
+
+	return (0);
+
+}
